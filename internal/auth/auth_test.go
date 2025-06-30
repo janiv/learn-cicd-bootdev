931f18d3c5c6434829af686bae97ae76f6901317
janiv <jani.vilkki@gmail.com>
2025-06-30 03:03:48 -0500

    fixed logic error in apikey test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,28 +7,29 @@ import (
 	"testing"
 )
 
-var headerWithMissingKey = http.Header{
-	"Content-Type": {"application/json"},
-}
-var headerWithKey = http.Header{
-	"Content-Type":  {"application/json"},
-	"Authorization": {"ApiKey 007secretkey"},
-}
-
 func TestGetAPIKey(t *testing.T) {
+
+	var headerWithMissingKey = http.Header{
+		"Content-Type": {"application/json"},
+	}
+	var headerWithKey = http.Header{
+		"Content-Type":  {"application/json"},
+		"Authorization": {"ApiKey 007secretkey"},
+	}
 	cases := []struct {
 		input http.Header
 		key   string
 		err   error
 	}{
 		{input: headerWithMissingKey, key: "", err: ErrNoAuthHeaderIncluded},
-		{input: headerWithKey, key: "007Knownretkey", err: nil},
+		{input: headerWithKey, key: "WRONGKEY", err: nil},
 	}
 
 	for i, c := range cases {
 		t.Run(fmt.Sprintf("Test case %v", i), func(t *testing.T) {
 			output, outErr := GetAPIKey(c.input)
-			if output != c.key && !errors.Is(c.err, outErr) {
+			fmt.Printf("output:%s outErr: %v c.key:%v c.err:%v\n", output, outErr, c.key, c.err)
+			if output != c.key || !errors.Is(c.err, outErr) {
 				t.Errorf("test case %v failed, got %v:%v instead of %v:%v", i, output, outErr, c.key, c.err)
 			}
 		})
